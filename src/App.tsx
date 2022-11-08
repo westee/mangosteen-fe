64780021d904e0cc490e0847c94830fdaaad0905
laowang <904885508@qq.com>
2022-11-08 23:39:45 +0800

    修改版本；引入vue-router

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { defineComponent, ref } from "vue";
+import { RouterView } from "vue-router";
+
+export default defineComponent({
+  setup() {
+    const refCount = ref(0);
+    const onClick = () => {
+      refCount.value += 1;
+    };
+    return () => (
+      <>
+        <header>
+          导航
+          <ul>
+            <li>
+              <router-link to="/">Foo</router-link>
+            </li>
+            <li>
+              <router-link to="/about">Bar</router-link>
+            </li>
+          </ul>
+        </header>
+        <div>
+          <RouterView />
+        </div>
+        <footer>页脚</footer>
+      </>
+    );
+  },
+});

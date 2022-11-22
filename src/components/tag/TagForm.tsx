81ab2832adac046623b6690c6e8c5f0f3f33c37b
laowang <904885508@qq.com>
2022-11-22 16:21:17 +0800

    完成创建标签

--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -1,8 +1,11 @@
 import { defineComponent, PropType, reactive, toRaw } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import { Button } from '../../shared/Button';
 import { EmojiSelect } from '../../shared/EmojiSelect';
 import { Form, FormItem } from '../../shared/Form';
-import { Rules, validate } from '../../shared/validate';
+import { http } from '../../shared/Http';
+import { onFormError } from '../../shared/onFormError';
+import { hasError, Rules, validate } from '../../shared/validate';
 import s from './Tag.module.scss';
 export const TagForm = defineComponent({
   props: {
@@ -15,23 +18,33 @@ export const TagForm = defineComponent({
       name: '',
       sign: '',
     })
+    const router = useRouter();
+    const route = useRoute();
     const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
-    const onSubmit = (e: Event) => {
+    const onSubmit = async (e: Event) => {
       const rules: Rules<typeof formData> = [
         { key: 'name', type: 'required', message: '必填' },
         { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
         { key: 'sign', type: 'required', message: '必填' },
       ]
       Object.assign(errors, {
-        name: undefined,
-        sign: undefined
+        name: [],
+        sign: []
       })
       Object.assign(errors, validate(formData, rules))
       e.preventDefault()
+      if(!hasError(errors)){
+        const response = await http.post('/tags', formData, {
+          params: {_mock: 'tagCreate'},
+        }).catch((error)=>
+          onFormError(error, (data)=> Object.assign(errors, data.errors))
+        )
+        router.back()
+      }
     }
     return () => (
       <Form onSubmit={onSubmit}>
-        <FormItem label='标签名'
+        <FormItem label='标签名（最多 4 个字符）'
           type="text"
           v-model={formData.name}
           error={errors['name']?.[0]} />
@@ -42,7 +55,7 @@ export const TagForm = defineComponent({
           <p class={s.tips}>记账时长按标签即可进行编辑</p>
         </FormItem>
         <FormItem>
-          <Button class={[s.button]}>确定</Button>
+          <Button type="submit" class={[s.button]}>确定</Button>
         </FormItem>
       </Form>
     )
--- src/shared/validate.tsx
+++ src/shared/validate.tsx
@@ -46,7 +46,7 @@ interface FData {
   export function hasError(errors: Record<string, string[]>) {
     let result = false;
     for (let key in errors) {
-      if(errors[key].length !== 0) {
+      if(errors[key]?.length !== 0) {
         result = true;
         break;
       }

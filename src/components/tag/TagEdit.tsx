5d80b4935f1e6a68ce99f049e5545bc3ae7fbacd
laowang <904885508@qq.com>
2022-11-22 19:55:39 +0800

    增加删除标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,9 +1,11 @@
+import { Dialog } from 'vant';
 import { defineComponent, reactive } from 'vue';
-import { useRoute } from 'vue-router';
+import { routerKey, useRoute, useRouter } from 'vue-router';
 import { MainLayout } from '../../layouts/MainLayout';
 import { BackIcon } from '../../shared/BackIcon';
 import { Button } from '../../shared/Button';
 import { EmojiSelect } from '../../shared/EmojiSelect';
+import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
 import { Rules, validate } from '../../shared/validate';
 import s from './Tag.module.scss';
@@ -14,7 +16,22 @@ export const TagEdit = defineComponent({
       name: '',
       sign: '',
     })
+    const onError = ()=>{
+      Dialog.alert({ title:'提示',message:'删除失败' })
+    }
+    const router = useRouter()
+
     const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
+    const onDelete = async (options?: {withItems?: boolean}) => {
+      await Dialog.confirm({
+        title: '确认',
+        message:'你真的要删除吗？'
+      })
+      await http.delete(`/tags/${numberId}`, {
+        withItems: options?.withItems ? 'true' : 'false'
+      }).catch(onError)
+      router.back()
+    }
     const onSubmit = (e: Event) => {
       const rules: Rules<typeof formData> = [
         { key: 'name', type: 'required', message: '必填' },
@@ -37,14 +54,15 @@ export const TagEdit = defineComponent({
       <MainLayout>{{
         title: () => '编辑标签',
         icon: <BackIcon />,
-        default: () => <>
-          <TagForm id={numberId} />
+        default: () => <>---
+          <TagForm id={numberId} />111
           <div class={s.actions}>
-            <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
-            <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+            <Button level='danger' class={s.removeTags} onClick={() => onDelete()}>删除标签</Button>
+            <Button level='danger' class={s.removeTagsAndItems} onClick={()=>onDelete({withItems: true})}>删除标签和记账</Button>
           </div>
         </>
       }}</MainLayout>
     )
   }
 })
+
